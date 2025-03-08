08b8d46fd19889cc7da7e6149c29f3ead7df7ff0
NguyenDenn <doancongdat00@gmail.com>
2025-03-08 14:08:13 +0700

    Hiển thị product và format number với Intl

--- ShopeeClone/src/Pages/ProductList/Product/Product.tsx
+++ ShopeeClone/src/Pages/ProductList/Product/Product.tsx
@@ -1,29 +1,28 @@
 import { Link } from 'react-router-dom'
+import { Product as ProductType } from '~/types/product.type'
+import { formatCurrency, formatNumberToSocialStyle } from '~/utils/utils'
 
-export default function Product() {
+interface Props {
+  product: ProductType
+}
+export default function Product({ product }: Props) {
   return (
     <Link to='/'>
       <div className='bg-white shadow rounded-sm hover:translate-y-[-0.0625rem] hover:shadow-sm duration-100 transition-transform overflow-hidden'>
         <div className='w-full pt-[100%] relative'>
-          <img
-            className='absolute top-0 left-0 bg-white w-full h-full object-cover'
-            src='https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lq7q2fcxl92vae_tn'
-          />
+          <img className='absolute top-0 left-0 bg-white w-full h-full object-cover' src={product.image} />
         </div>
         <div className='p-2 overflow-hidden'>
-          <div className='min-h-[2rem] line-clamp-2 text-xs'>
-            Áo khoác nỉ hoodie zip nam nữ local brand unisex bigsize bông dầy dặn dây kéo có mũ to mặc cặp đôi Pandax
-            Polime Race
-          </div>
+          <div className='min-h-[2rem] line-clamp-2 text-xs'>{product.name}</div>
 
           <div className='flex items-center mt-3 '>
             <div className='line-through max-w-[50%] text-gray-500 truncate'>
               <span className='text-xs'>₫</span>
-              <span>150.000</span>
+              <span>{formatCurrency(product.price_before_discount)}</span>
             </div>
             <div className='max-w-[50%] text-orange truncate ml-1'>
               <span className='text-xs'>₫</span>
-              <span>100.000</span>
+              <span>{formatCurrency(product.price)}</span>
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
@@ -62,7 +61,7 @@ export default function Product() {
               </div>
             </div>
             <div className='ml-2 text-sm'>
-              <span>5.66k</span>
+              <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>
             </div>
           </div>
